dbcc3249c5a68c0e88db3eb163f32a5da913f061
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-02-20 19:16:57 +0530

    Quiz array done

---
+++ Quiz-App/script.js
@@ -0,0 +1,43 @@
+const quiz = [
+    {
+        question: 'Which one is the smallest ocean in the World?',
+        a:'Indian',
+        b:'Pacific',
+        c:'Atlantic',
+        d:'Arctic',
+        answer:'d'
+    },
+    {
+        question:'Which country gifted the ‘Statue of Liberty’ to USA in 1886?',
+        a:'France',
+        b:'Canada',
+        c:'Brazil',
+        d:'England',
+        answer:'a'
+    },
+    {
+        question:'Dead Sea is located between which two countries?',
+        a:'Jordan and Sudan',
+        b:'Jordan and Israel',
+        c:'Turkey and UAE',
+        d:'UAE and Egypt',
+        answer:'b'
+    },
+    {
+        question:'In which ocean Bermuda Triangle region is located?',
+        a:'Atlantic',
+        b:'Indian',
+        c:'Pacific',
+        d:'Arctic',
+        answer:'a'
+    },
+    {
+        question:'Which continent has the highest number of countries?',
+        a:'Asia',
+        b:'Europe',
+        c:'North America',
+        d:'Africa',
+        answer:'d'
+    }
+]
+
