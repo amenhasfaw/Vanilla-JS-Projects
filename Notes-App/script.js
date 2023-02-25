92c4ab34cfeebcd627a3867689bd5dd8d4f9e7df
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-02-25 10:51:52 +0530

    Structure done

---
+++ Notes-App/script.js
@@ -0,0 +1,18 @@
+
+
+/* <div id="note">
+  <div id="note-edit">
+    <button>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1828/1828802.png"
+        alt="Edit Button"
+      />
+    </button>
+    <button>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/484/484560.png"
+        alt="Delete Button"
+      />
+    </button>
+  </div>
+</div> */
