c5bd4ec4b21280cb6004a1a921a6e8d758e9bfde
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-03-02 02:26:57 +0530

    update

--- Movie-App/script.js
+++ Movie-App/script.js
@@ -1,7 +1,7 @@
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+let IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
+const ALT_POSTER = "https://ih1.redbubble.net/image.1027712254.9762/fposter,small,wall_texture,product,750x1000.u2.jpg";
 
 const main = document.getElementById('movies');
 const form = document.getElementById('form');
@@ -16,13 +16,24 @@ async function getMovies(url){
     console.log(response.results)
 
     response.results.forEach(movie => {
-       const {title, poster_path, vote_average, overview, release_date } = movie
+       const {id, title, vote_average, overview, release_date } = movie
+       let {poster_path} = movie
+
+
 
        const movieEL = document.createElement('div')
        movieEL.classList.add('main')
 
+       if(poster_path == null){
+        poster_path = ALT_POSTER
+        IMGPATH = ""
+       }
+
+       console.log(poster_path)
+
+
        movieEL.innerHTML = `
-       <img src="${IMGPATH + poster_path}" alt="">
+       <img id='${id}' src="${IMGPATH + poster_path}">
        <div id="movie-info">
            <h2>${title}</h2>
            <span>${vote_average.toFixed(1)}</span>
